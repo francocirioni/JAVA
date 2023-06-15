b9e998e0ff431949d2bb47838742cd036c2be800
francocirioni <101602566+francocirioni@users.noreply.github.com>
2023-06-15 12:14:06 -0300

    2

---
+++ TryCatchdivicion.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class TryCatchdivicion {
+
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner (System.in);
+    System.out.print("dame un numero");
+    int numero1 = scanner.nextInt();
+    System.out.print("dame otro numero");
+    int numero2 = scanner.nextInt();
+
+
+    try{   
+        int resultado = numero1 % numero2  ; 
+        System.out.print("el resultado es " + resultado);}
+
+    catch (ArithmeticException exception)
+    {     System.out.print("no se puede dividir"); }
+
+
+ 
+
+  
+
+    scanner.close();
+    }
+    
+}
